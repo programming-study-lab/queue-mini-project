d083781f801346947bc3cda59c5adf91f1b3e7e6
your-auction-services <anaphat.wo@rmuti.ac.th>
2025-02-08 19:35:18 +0700

    08/02/2568

--- api-queue/app/Http/Controllers/api/v1/AuthController.php
+++ api-queue/app/Http/Controllers/api/v1/AuthController.php
@@ -3,20 +3,61 @@
 namespace App\Http\Controllers\api\v1;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
+use App\Http\Resources\QueueDatasResource;
+use App\Models\QueueDatas;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
     public function register(Request $request) {
-        $queue = User::create([
-            "name" => "test"
+        $validator = Validator::make($request->all(),[ //เป็นคลาสของ Laravel
+            'first_name' => 'string|max:5',
+            'last_name' => 'string|nullable',
+            'phone' => 'string|nullable',
+            'type' => 'string|nullable',
+            'queue_status' => 'nullable',
+            'queue_number' => 'nullable',
         ]);
 
+        $first_name = $request->first_name;
+        $last_name = $request->last_name;
+        $phone = $request->phone;
+        $type = $request->type;
+        $queue_status = $request->queue_status;
+        $queue_number = $request->queue_number;
+
+        if ($validator->fails()) {
+            return response()->json([
+                $validator->errors()
+            ]);
+        }
+
+        if ($last_name == null) {
+            $last_name = "";
+        }
+
+        $queue = QueueDatas::create([
+            'first_name' => $first_name,
+            'last_name' => $last_name,
+            'phone' => $phone,
+            'type' => $type,
+            'queue_status' => $queue_status,
+            'queue_number' => $queue_number,
+        ]);
 
         return response()->json([
             "status" => "ok",
-            // "queue" => $queue
+            "queue" => $queue
         ]);
+    }
+
+    public function queue(){
+        $queue_datas = QueueDatas::latest()->get();
+
+        return response()->json([
+            'status' => 'ok',
+            'queue' => QueueDatasResource::collection($queue_datas)
+        ], 200);
     }
 }
